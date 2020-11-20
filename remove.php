c01c9a09159d567d77ecef6d9213c84de1f677c1
Yuttana Phongphuak <73682068+it63070143@users.noreply.github.com>
2020-11-20 16:45:53 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -12,7 +12,7 @@
         <?php
 
             $conn = mysqli_init();
-            mysqli_real_connect($conn, -, -, -, 'itflab', 3306);
+            mysqli_real_connect($conn, 'labkmitl13.mysql.database.azure.com', 'arm143@labkmitl13', 'it-63070143', 'itflab', 3306);
             if (mysqli_connect_errno($conn))
             {
                 die('Failed to connect to MySQL: '.mysqli_connect_error());
