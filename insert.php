bdb0dcaa4ba321ff0a657a661c94c725d577e046
Yuttana Phongphuak <73682068+it63070143@users.noreply.github.com>
2020-11-20 17:01:59 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,7 +17,7 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "<h2 style='text-align:center; color:white;'>New record created successfully</h2>";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
